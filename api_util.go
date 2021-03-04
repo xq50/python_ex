bcab3c07f1896efdb2b1446361a45bb01c91db68
xq50 <107172640+xq50@users.noreply.github.com>
2021-03-04 16:08:02 +0000

    removed target

--- api_util.go
+++ api_util.go
@@ -25,3 +25,6 @@ var (
 	MINUTES_15 HistoryInterval = HistoryInterval{"m15", time.Minute * 15}
 	MINUTES_30 HistoryInterval = HistoryInterval{"m30", time.Minute * 30}
 	HOURS_1    HistoryInterval = HistoryInterval{"h1", time.Hour}
+	HOURS_2    HistoryInterval = HistoryInterval{"h2", time.Hour * 2}
+	HOURS_6    HistoryInterval = HistoryInterval{"h6", time.Hour * 6}
+	HOURS_12   HistoryInterval = HistoryInterval{"h12", time.Hour * 12}
